1d6fe81544ef62f454adecb62018b93a8b7a4e51
Zack Bloom <zbloom@hubspot.com>
2014-04-07 02:39:47 -0400

    Add explicit headers

--- node/server.js
+++ node/server.js
@@ -11,7 +11,7 @@ var APP_ROOT = process.env.APP_ROOT || '/';
 var cors = function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
 
   next();
 }
